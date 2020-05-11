b637f34290c96bab614b7a4b5ba3eef266f3c230
readingradio <will1809@purdue.edu>
2020-05-10 23:05:24 -0400

    Network complexity figures changes

--- Net.complex.functions.R
+++ Net.complex.functions.R
@@ -1,12 +1,14 @@
 library(tidyverse)
 library(igraph)
 library(tidygraph)
+library(ggraph)
 library(graph)
 library(QuACN)
 library(acss)
 library(doParallel)
 library(foreach)
 library(doBy)
+library(Hmisc)
 
 setwd("OACC")
 source("scripts/BDM2D.R")
@@ -99,8 +101,99 @@ network.complexity.plot <- function (x, stat=c("Kolmogorov", "Entropy", "Complex
 	legend(leg, legend=c("IN","TN","WA","R=0.8","R=0.6"), lty=c(1,2,3,NA,NA), pch=c(NA,NA,NA,1,2))
 }
 
-net.complex.multipanel <- function (x) {
+net.complex.multipanel <- function (x,which.leg="last") {
 	par(mfrow=c(2,3))
+	if(which.leg=="last"){
 	for (i in c("Kolmogorov", "Entropy", "Complex.Index", "Norm.Edge.Complex", "Shannon.ent")) network.complexity.plot(x, i)
 	network.complexity.plot(x, "Shannon.2nd", leg="topleft")
+	} else if (which.leg=="first") {
+	network.complexity.plot(x, "Kolmogorov", leg="topleft")
+	for (i in c("Entropy", "Complex.Index", "Norm.Edge.Complex", "Shannon.ent", "Shannon.2nd")) network.complexity.plot(x, i)
+	}
 }
+
+
+# function to create a network object
+
+make.network.graph <- function (p, r, s, subs=F, tissue="shavings") {
+
+  gr <- read.csv(paste("Net.analysis/CorrNetworks.", s,"/cyto.", tissue, ".merged.p.", p, ".r.", r,".txt", sep = ''), sep='\t')[,1:3] %>% as.matrix() %>% graph_from_data_frame(directed =F)
+  
+  edge_attr(gr, name= 'r.sign') <- read.csv(paste("Net.analysis/CorrNetworks.", s,"/cyto.", tissue, ".merged.p.", p, ".r.", r,".txt", sep = ''), sep='\t')[,3] %>% sign() %>% as.factor()
+  edge_attr(gr, name= 'r.abs') <- read.csv(paste("Net.analysis/CorrNetworks.", s,"/cyto.", tissue, ".merged.p.", p, ".r.", r,".txt", sep = ''), sep='\t')[,3] %>% abs() %>% as.numeric()
+  vertex_attr(gr, name='deg') <- igraph::degree(gr)
+  vertex_attr(gr, name='fu.ba') <- (vertex_attr(gr, 'name') %>% grepl('Botu',.) ) %>% as.numeric()
+
+  bd.tab <- read.csv(paste("Results/", capitalize(tissue), ".HubOTUS.", s, ".csv", sep=""), row.names = 1)
+  vertex_attr(gr, name='hub') <- vertex_attr(gr, 'name') %in% rownames(bd.tab)[bd.tab[, paste("p",p,".r",r,sep="")] == 1] %>% as.numeric()
+  
+  if (s == "WA") {
+    vertex_attr(gr, name='hub') <- ((((vertex_attr(gr, 'name') == 'Otu0115' )) %>% as.numeric) + vertex_attr(gr, name='hub'))
+    vertex_attr(gr, name='fu.ba') <- ((((vertex_attr(gr, 'name') == 'Otu0115' )) %>% as.numeric)*2 + vertex_attr(gr, name='fu.ba'))
+  }
+  
+  if (!subs) {
+    dec.subrs <- decompose.graph(gr)
+    cl.gr.i <- which.max(clusters(gr)$csize)
+    gr <- dec.subrs[[cl.gr.i]]
+  }
+  
+  gr
+}
+
+## create object for chosen caulosphere or soil graph (can switch between TN and WA)
+
+top.clusters <- function (p='0.01', r='0.8', state, tissue="shavings") {
+	g.r0.8.p0.01 <- make.network.graph(p, r, state, tissue=tissue)
+
+	# remove negative connections
+	# as.factor(sign(c(1,-8)))
+
+	negs<-edge_attr(g.r0.8.p0.01, name= 'r.sign') == 1
+	gr.r8p01.pos <-delete_edges(g.r0.8.p0.01, which(negs))
+
+	# pull out major subnetworks
+
+	dec.subrs.r8p01 <- decompose.graph(gr.r8p01.pos)
+	clusters(gr.r8p01.pos)
+	twobiggest <- rev(order(clusters(gr.r8p01.pos)$csize))[1:2]
+	cluster.1 <- dec.subrs.r8p01[[twobiggest[1]]]
+	cluster.2 <- dec.subrs.r8p01[[twobiggest[2]]]
+	
+	vertex_attr(g.r0.8.p0.01, "Cluster") <- (0 + 1*(clusters(gr.r8p01.pos)$membership == twobiggest[1]) + 2*(clusters(gr.r8p01.pos)$membership == twobiggest[2])) %>% as.factor()
+
+	edge_attr(g.r0.8.p0.01, "ClusterEdge") <- (0 + 1*(attr(E(g.r0.8.p0.01),"vnames") %in% attr(E(cluster.1),"vnames")) + 2*(attr(E(g.r0.8.p0.01),"vnames") %in% attr(E(cluster.2),"vnames"))) %>% as.factor()
+	
+	list(cluster.1=cluster.1, cluster.2=cluster.2, net.annot=g.r0.8.p0.01)
+
+}
+
+figure.network <- function (p, r, s, subs=F, subcolors=NULL, annot.obj=NULL, tissue="shavings") {
+
+  if (is.null(annot.obj)) gr <- make.network.graph(p, r, s, subs=F, tissue=tissue)
+  else gr <- annot.obj
+  
+  layout <- layout_with_kk(gr, weights=2-as.numeric(edge_attr(gr, 'r')))
+  
+  if(is.null(subcolors))
+	ggraph(gr, layout) + 
+    geom_edge_link(aes(width=.1, alpha=r.abs, color=as.factor(r.sign))) + 
+    scale_edge_color_manual(values=c('grey','blue')) + 
+    scale_edge_width(range=c(0.2,1)) + 
+    geom_node_point(aes(size=deg, shape=as.factor(hub), color=as.factor(fu.ba))) + 
+    scale_size(range = c(.2,2.5)) + scale_color_manual(values=c('red','black','green')) +
+    scale_shape_manual(values=c(1,19,8)) + 
+    theme_graph() + 
+    theme(legend.position='none', plot.margin=unit(c(0,0,0,0),"cm"))
+  else
+	ggraph(gr, layout) + 
+    geom_edge_link(aes(width=.1, alpha=r.abs, color=as.factor(ClusterEdge))) + 
+    scale_edge_color_manual(values=c('grey',subcolors)) + 
+    scale_edge_width(range=c(0.2,1)) + 
+    geom_node_point(aes(size=deg, shape=as.factor(hub), color=as.factor(Cluster))) + 
+    scale_size(range = c(.2,2.5)) + scale_color_manual(values=c('grey',subcolors)) +
+    scale_shape_manual(values=c(1,19,8)) + 
+    theme_graph() + 
+    theme(legend.position='none', plot.margin=unit(c(0,0,0,0),"cm"))
+}
+
--- Network.GMW.Jnigra.caulo.Gm.analyze.allstates.R
+++ Network.GMW.Jnigra.caulo.Gm.analyze.allstates.R
@@ -9,6 +9,10 @@ library(ggraph)
 library(gridExtra)
 library(QuACN)
 library(acss)
+library(phyloseq)
+library(vegan)
+library(reshape2)
+library(tanagR)
 
 source("Net.complex.functions.R")
 
@@ -145,16 +149,16 @@ intersect(wa.hubs$name, in.hubs$name) %>% intersect(tn.hubs$name)
 library(VennDiagram)
 
 venn.diagram(x = list(in.hubs$name, wa.hubs$name, tn.hubs$name), category.names=c("IN","WA","TN"), filename="Figures/Shavings.Net.Venn.png", output=T)
- 
-## figures
 
-figure.network <- function (p, r, s, subs=F) {
+# function to create a network object
+
+make.network.graph <- function (p, r, s, subs=F) {
 
   gr <- read.csv(paste("Net.analysis/CorrNetworks.", s,"/cyto.shavings.merged.p.", p, ".r.", r,".txt", sep = ''), sep='\t')[,1:3] %>% as.matrix() %>% graph_from_data_frame(directed =F)
   
   edge_attr(gr, name= 'r.sign') <- read.csv(paste("Net.analysis/CorrNetworks.", s,"/cyto.shavings.merged.p.", p, ".r.", r,".txt", sep = ''), sep='\t')[,3] %>% sign() %>% as.factor()
   edge_attr(gr, name= 'r.abs') <- read.csv(paste("Net.analysis/CorrNetworks.", s,"/cyto.shavings.merged.p.", p, ".r.", r,".txt", sep = ''), sep='\t')[,3] %>% abs() %>% as.numeric()
-  vertex_attr(gr, name='deg') <- degree(gr)
+  vertex_attr(gr, name='deg') <- igraph::degree(gr)
   vertex_attr(gr, name='fu.ba') <- (vertex_attr(gr, 'name') %>% grepl('Botu',.) ) %>% as.numeric()
   
   bd.tab <- read.csv(paste("Results/Shavings.HubOTUS.", s, ".csv", sep=""), row.names = 1)
@@ -171,9 +175,138 @@ figure.network <- function (p, r, s, subs=F) {
     gr <- dec.subrs[[cl.gr.i]]
   }
   
+  gr
+}
+
+## create object for chosen caulosphere graph (can switch between TN and WA)
+
+top.clusters <- function (p='0.01', r='0.8', state) {
+	g.r0.8.p0.01 <- make.network.graph(p, r, state)
+
+	# remove negative connections
+	# as.factor(sign(c(1,-8)))
+
+	negs<-edge_attr(g.r0.8.p0.01, name= 'r.sign') == 1
+	gr.r8p01.pos <-delete_edges(g.r0.8.p0.01, which(negs))
+
+	# pull out major subnetworks
+
+	dec.subrs.r8p01 <- decompose.graph(gr.r8p01.pos)
+	clusters(gr.r8p01.pos)
+	twobiggest <- rev(order(clusters(gr.r8p01.pos)$csize))[1:2]
+	cluster.1 <- dec.subrs.r8p01[[twobiggest[1]]]
+	cluster.2 <- dec.subrs.r8p01[[twobiggest[2]]]
+	
+	vertex_attr(g.r0.8.p0.01, "Cluster") <- (0 + 1*(clusters(gr.r8p01.pos)$membership == twobiggest[1]) + 2*(clusters(gr.r8p01.pos)$membership == twobiggest[2])) %>% as.factor()
+
+	edge_attr(g.r0.8.p0.01, "ClusterEdge") <- (0 + 1*(attr(E(g.r0.8.p0.01),"vnames") %in% attr(E(cluster.1),"vnames")) + 2*(attr(E(g.r0.8.p0.01),"vnames") %in% attr(E(cluster.2),"vnames"))) %>% as.factor()
+	
+	list(cluster.1=cluster.1, cluster.2=cluster.2, net.annot=g.r0.8.p0.01)
+
+}
+
+wa12 <- top.clusters(state='WA')
+tn12 <- top.clusters(state='TN')
+
+# just hub OTUs from the two clusters 
+wa.clust.1.otus <- with(wa12,vertex_attr(cluster.1,"name")[as.logical(vertex_attr(cluster.1,"hub"))])
+wa.clust.2.otus <- with(wa12,vertex_attr(cluster.2,"name")[as.logical(vertex_attr(cluster.2,"hub"))])
+
+tn.clust.1.otus <- with(tn12,vertex_attr(cluster.1,"name")[as.logical(vertex_attr(cluster.1,"hub"))])
+tn.clust.2.otus <- with(tn12,vertex_attr(cluster.2,"name")[as.logical(vertex_attr(cluster.2,"hub"))])
+
+# Trees that tested positive for GM with the probe were WA55RN1, WA130RN4, and WA132RN9. There were five other trees that only tested positive once. Those include the WA132 (the remaining two), WA272RN10, and WAWTBNL22, WAWTBNL23
+# rownames(shavings.merge.df[,clust.1.otus])
+
+wa.gm <- c("WA_RN1_55s_S44","WA_RN4_130s_S21","WA_RN9_132s_S61","WA_RN8_132s_S53","WA_RN7_132s_S37","WA_BNL23_WTs_S6","WA_BNL22_WTs_S45")
+wa.neg<- c("WA_BNL18_272s_S85","WA_BNL19_55s_S13","WA_BNL21_WTs_S93")
+
+# Trees in TCD positive and negative location in TN
+
+tn.tcd <- rownames(shavings.merge.df)[regexpr("TN_LS",rownames(shavings.merge.df),perl=T)==1]
+tn.neg <- rownames(shavings.merge.df)[regexpr("TN",rownames(shavings.merge.df),perl=T)==1] %>% setdiff(tn.tcd)
+
+indian <- rownames(shavings.merge.df)[regexpr("IN_",rownames(shavings.merge.df),perl=T)==1] %>% setdiff(tn.tcd)
+
+intersect(wa.clust.1.otus,tn.clust.1.otus)
+intersect(wa.clust.1.otus,tn.clust.2.otus)
+intersect(wa.clust.2.otus,tn.clust.1.otus)
+intersect(wa.clust.2.otus,tn.clust.2.otus)
+
+shavings.merge.df[] <- lapply(shavings.merge.df, function(x) as.numeric(as.character(x)))
+
+# cluster 1 and 2 for gm
+shavings.merge.df[wa.gm,wa.clust.1.otus] %>% t() %>% cbind(tax_table(shavings.merge)[wa.clust.1.otus],.) %>% as.data.frame()
+shavings.merge.df[wa.gm,wa.clust.2.otus] %>% t() %>% cbind(tax_table(shavings.merge)[wa.clust.2.otus],.) %>% as.data.frame()
+
+# cluster 1 and 2 for neg trees
+shavings.merge.df[wa.neg,wa.clust.1.otus] %>% t() %>% cbind(tax_table(shavings.merge)[wa.clust.1.otus],.) %>% as.data.frame()
+shavings.merge.df[wa.neg,wa.clust.2.otus] %>% t() %>% cbind(tax_table(shavings.merge)[wa.clust.2.otus],.) %>% as.data.frame()
+
+# cluster 1  for tn knox and tn polk
+shavings.merge.df[tn.tcd,tn.clust.1.otus] %>% t() %>% cbind(tax_table(shavings.merge)[tn.clust.1.otus],.) %>% as.data.frame()
+shavings.merge.df[tn.neg,tn.clust.1.otus] %>% t() %>% cbind(tax_table(shavings.merge)[tn.clust.1.otus],.) %>% as.data.frame()
+
+# cluster 2 for tn tn knox and tn polk
+shavings.merge.df[tn.tcd,tn.clust.2.otus] %>% t() %>% cbind(tax_table(shavings.merge)[tn.clust.2.otus],.) %>% as.data.frame()
+shavings.merge.df[tn.neg,tn.clust.2.otus] %>% t() %>% cbind(tax_table(shavings.merge)[tn.clust.2.otus],.) %>% as.data.frame()
+
+# combine wa cluster 1 and 2, tn cluster 1 and 2
+
+hm <- shavings.merge.df[c(indian,tn.neg,tn.tcd,wa.neg,wa.gm),c(tn.clust.1.otus,tn.clust.2.otus,wa.clust.1.otus,wa.clust.2.otus)] %>% t() %>% cbind(tax_table(shavings.merge)[c(tn.clust.1.otus,tn.clust.2.otus,wa.clust.1.otus,wa.clust.2.otus)],.) %>% as.data.frame()
+
+hm[,-c(1:7)] <- lapply(hm[,-c(1:7)], function(x) as.numeric(as.character(x)))
+
+#hm.melt <- (1-(hm[,-c(1:7)] %>% decostand(method="log") %>% decostand(.,method="max", MARGIN=1))) %>% cbind(data.frame(OTU=rownames(hm), Cluster = c(rep(1,length(tn.clust.1.otus)),rep(2,length(tn.clust.2.otus)),rep(3,length(wa.clust.1.otus)),rep(4,length(wa.clust.2.otus))))) %>% melt(id.vars=c("OTU","Cluster"),variable.name="Sample") 
+
+hm.melt <- hm[,-c(1:7)] %>% decostand(method="log") %>% decostand(.,method="max", MARGIN=1) %>% cbind(data.frame(OTU=rownames(hm), Cluster = as.factor(c(rep(1,length(tn.clust.1.otus)),rep(2,length(tn.clust.2.otus)),rep(3,length(wa.clust.1.otus)),rep(4,length(wa.clust.2.otus)))))) %>% melt(id.vars=c("OTU","Cluster"),variable.name="Sample") 
+
+hm.melt$Sample <- factor(hm.melt$Sample, c(indian,tn.neg,tn.tcd,wa.neg,wa.gm))
+
+hm.melt$OTU <- factor(hm.melt$OTU, rev(c(tn.clust.1.otus,tn.clust.2.otus,wa.clust.1.otus,wa.clust.2.otus)))
+
+axislabels <- with(hm, paste(rownames(hm), gsub("[sgfocp]__","",Genus,perl=T) %>% gsub("_"," ",.,perl=T) %>% gsub("[\\s]?(unclassified|ge)[\\s]?","",.,perl=T)%>% gsub("\\([A-Za-z0-9 _]*\\)"," ",.,perl=T)))
+names(axislabels) <- rownames(hm)
+
+i <- grep("(uncultured$)|([01234567890-]+$)|(group$)", axislabels, perl=T)
+axislabels[i]<-paste(names(axislabels)[i], hm$Family[i])
+j <- grep("(uncultured$)|([01234567890-]+$)|(group$)|([01234567890-]+_fa$)", axislabels, perl=T)
+axislabels[j]<-paste(names(axislabels)[j], hm$Order[j])
+k <- grep("(uncultured_fa$)|([01234567890-]+_or$)", axislabels, perl=T)
+axislabels[k]<-paste(names(axislabels)[k], hm$Phylum[k])
+l <- grep("[01234567890-]+$", axislabels, perl=T)
+axislabels[l]<-paste(names(axislabels)[l], hm$Class[l])
+
+samplelabels<-c(rep("Indiana", length(indian)),rep("TN Polk Co. Clones", length(tn.neg)-3),rep("TN Polk Co. Bedrun", 3),rep("TN Knox Co. Wild", length(tn.tcd)),rep("WA TCD-negative", length(wa.neg)),rep("WA TCD-positive", length(wa.gm)))
+
+colors.subnetworks<-tanagr_palette("dacnis_berlepschi")[1:4]
+
+hm.melt %>% ggplot(., aes(x=Sample, y=OTU, fill=value)) + geom_tile() + scale_fill_gradient(low="white", high="black") + theme(axis.text.x = element_text(angle=90, size=6), legend.position = "none", axis.text.y = element_text(hjust=0))+ scale_y_discrete(labels=axislabels)
+
+heatmap.plot <- hm.melt %>% ggplot(., aes(x=Sample, y=OTU, fill=Cluster, alpha=value)) + geom_tile() + scale_fill_manual(values=colors.subnetworks) + scale_alpha_continuous(range=c(0,1)) + scale_y_discrete(labels=axislabels) + scale_x_discrete(labels=samplelabels)+
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=90, size=6),
+  legend.position = "none",
+  axis.text.y = element_text(hjust=0),
+  axis.line = element_line(colour = "black"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()) 
+
+## function to create netwrok graph object
+
+figure.network <- function (p, r, s, subs=F, subcolors=NULL, annot.obj=NULL) {
+
+  if (is.null(annot.obj)) gr <- make.network.graph(p, r, s, subs=F, subcolors=subcolors)
+  else gr <- annot.obj
+  
   layout <- layout_with_kk(gr, weights=2-as.numeric(edge_attr(gr, 'r')))
   
-  ggraph(gr, layout) + 
+  if(is.null(subcolors))
+	ggraph(gr, layout) + 
     geom_edge_link(aes(width=.1, alpha=r.abs, color=as.factor(r.sign))) + 
     scale_edge_color_manual(values=c('grey','blue')) + 
     scale_edge_width(range=c(0.2,1)) + 
@@ -182,8 +315,23 @@ figure.network <- function (p, r, s, subs=F) {
     scale_shape_manual(values=c(1,19,8)) + 
     theme_graph() + 
     theme(legend.position='none')
-  
+  else
+	ggraph(gr, layout) + 
+    geom_edge_link(aes(width=.1, alpha=r.abs, color=as.factor(ClusterEdge))) + 
+    scale_edge_color_manual(values=c('grey',subcolors)) + 
+    scale_edge_width(range=c(0.2,1)) + 
+    geom_node_point(aes(size=deg, shape=as.factor(hub), color=as.factor(Cluster))) + 
+    scale_size(range = c(.2,2.5)) + scale_color_manual(values=c('grey',subcolors)) +
+    scale_shape_manual(values=c(1,19,8)) + 
+    theme_graph() + 
+    theme(legend.position='none')
 }
+
+## make side networks as a legend
+
+<- figure.network(subcolors = colors.subnetworks[1:2], annot.obj=tn12$net.annot)
+<- figure.network(subcolors = colors.subnetworks[3:4], annot.obj=wa12$net.annot)
+heatmap.plot
 
 for (p in c("0.05","0.01","0.005", "0.001")) {
   for (r in c("0.6","0.8")) {
